1a3c5218c53ca0f605eaeb3f7415ca5717477a5e
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-08-11 13:44:47 +0900

    example with real time ticker

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/adshao/go-binance/v2"
 )
@@ -18,6 +19,25 @@ func main() {
 	for _, p := range prices {
 		fmt.Println(p)
 	}
+
+	wsBookTickerHandler := func(event *binance.WsBookTickerEvent) {
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneChan, stopChan, err := binance.WsBookTickerServe("BTCUSDT", wsBookTickerHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// use stopC to exit
+	go func() {
+		time.Sleep(10 * time.Second)
+		stopChan <- struct{}{}
+	}()
+	// remove this if you do not want to be blocked here
+	<-doneChan
 
 	println("Hello, Gaya!")
 }
